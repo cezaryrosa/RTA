a9def80ce89a0fc440929a183d0f3e67652f74dc
cezaryrosa <50260574+cezaryrosa@users.noreply.github.com>
2022-05-17 08:31:33 +0200

    Update perceptron model.py

--- perceptron model.py
+++ perceptron model.py
@@ -23,3 +23,34 @@ class Perceptron:
     
     def predict(self, X):
         return np.where(self.net_input(X) >= 0, 1, -1)
+
+    import numpy as np
+import pandas as pd
+
+from sklearn.datasets import load_iris
+import joblib
+
+iris = load_iris()
+
+df = pd.DataFrame(data=np.c_[iris['data'], iris['target']],
+                  columns=iris['feature_names'] + ['target'])
+
+
+model = Perceptron()
+
+X, y = df.iloc[:100, [0,2]].values, df.iloc[:100, 4]
+
+def a(x):
+    if x == 0:
+        return -1
+    else:
+        return 1
+    
+y = y.map(a)
+
+iris['feature_names'] + ['target']
+y = y.values
+model.fit(X,y)
+model.w_
+model.predict([3.4, 3.1])
+joblib.dump(model, 'model.pkl')
